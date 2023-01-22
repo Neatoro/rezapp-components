b86eac65e29952eb155d1134c8a7776be6e1f0e1
Moritz Schramm <moritz.schramm@gmx.de>
2023-01-22 18:52:28 +0100

    Change to vite

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    lib: {
+      name: 'rezapp-components',
+      entry: resolve(__dirname, 'src', 'index.ts')
+    }
+  }
+})
